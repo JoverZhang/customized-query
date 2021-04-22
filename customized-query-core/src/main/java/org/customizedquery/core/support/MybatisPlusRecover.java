36ec0eb07a7d76858cdd04d9178a29c39817df86
joverzhang <joverzh@gmail.com>
2021-04-22 15:15:24 +0800

    Add validate of field for MybatisPlus

--- customized-query-core/src/main/java/org/customizedquery/core/support/MybatisPlusRecover.java
+++ customized-query-core/src/main/java/org/customizedquery/core/support/MybatisPlusRecover.java
@@ -1,6 +1,8 @@
 package org.customizedquery.core.support;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import lombok.SneakyThrows;
 import org.customizedquery.core.utils.Asserts;
 
@@ -15,15 +17,15 @@ import java.util.Map;
 public class MybatisPlusRecover {
 
     @SneakyThrows
-    public static <S extends AbstractRecoverableQuery, T> QueryWrapper<T> recover(S recoverable, Class<T> targetClass) {
+    public static <S extends AbstractRecoverableQuery, T> QueryWrapper<T> toWrapper(S recoverable, Class<T> targetClass) {
         Asserts.notNull(recoverable);
         Asserts.notNull(targetClass);
-        return doRecover(recoverable, targetClass);
+        return doToWrapper(recoverable, targetClass);
     }
 
     @SuppressWarnings("unchecked")
-    private static <S extends AbstractRecoverableQuery, T> QueryWrapper<T> doRecover(S recoverable,
-                                                                                     Class<T> targetClass)
+    private static <S extends AbstractRecoverableQuery, T> QueryWrapper<T> doToWrapper(S recoverable,
+                                                                                       Class<T> targetClass)
             throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
         QueryWrapper<T> wrapper = new QueryWrapper<>();
@@ -43,32 +45,32 @@ public class MybatisPlusRecover {
                     // eq
                     if (conditionName.endsWith("_" + Range.Compare.EQ.getAlias())) {
                         fieldName = conditionName.substring(0, conditionNameLen - 3);
-                        wrapper.eq(value != null, fieldName, value);
+                        wrapper.eq(value != null, fieldNameConvert(fieldName, targetClass), value);
                     }
                     // ne
                     else if (conditionName.endsWith("_" + Range.Compare.NE.getAlias())) {
                         fieldName = conditionName.substring(0, conditionNameLen - 3);
-                        wrapper.ne(value != null, fieldName, value);
+                        wrapper.ne(value != null, fieldNameConvert(fieldName, targetClass), value);
                     }
                     // gt
                     else if (conditionName.endsWith("_" + Range.Compare.GT.getAlias())) {
                         fieldName = conditionName.substring(0, conditionNameLen - 3);
-                        wrapper.gt(value != null, fieldName, value);
+                        wrapper.gt(value != null, fieldNameConvert(fieldName, targetClass), value);
                     }
                     // ge
                     else if (conditionName.endsWith("_" + Range.Compare.GE.getAlias())) {
                         fieldName = conditionName.substring(0, conditionNameLen - 3);
-                        wrapper.ge(value != null, fieldName, value);
+                        wrapper.ge(value != null, fieldNameConvert(fieldName, targetClass), value);
                     }
                     // lt
                     else if (conditionName.endsWith("_" + Range.Compare.LT.getAlias())) {
                         fieldName = conditionName.substring(0, conditionNameLen - 3);
-                        wrapper.lt(value != null, fieldName, value);
+                        wrapper.lt(value != null, fieldNameConvert(fieldName, targetClass), value);
                     }
                     // le
                     else if (conditionName.endsWith("_" + Range.Compare.LE.getAlias())) {
                         fieldName = conditionName.substring(0, conditionNameLen - 3);
-                        wrapper.le(value != null, fieldName, value);
+                        wrapper.le(value != null, fieldNameConvert(fieldName, targetClass), value);
                     }
                     // between
                     else if (conditionName.endsWith("_" + Range.Compare.BETWEEN.getAlias())) {
@@ -77,18 +79,18 @@ public class MybatisPlusRecover {
                         if (!(value instanceof List) || (values = (List<?>) value).size() < 2) {
                             throw new IllegalArgumentException();
                         }
-                        wrapper.between(fieldName, values.get(0), values.get(1));
+                        wrapper.between(fieldNameConvert(fieldName, targetClass), values.get(0), values.get(1));
                     }
                     // like right
                     else if (conditionName.endsWith("_" + Range.Compare.LIKE_RIGHT.getAlias())) {
                         fieldName = conditionName.substring(0, conditionNameLen - 10);
-                        wrapper.likeRight(value != null, fieldName, value);
+                        wrapper.likeRight(value != null, fieldNameConvert(fieldName, targetClass), value);
                     }
                 }
                 // Normal condition
                 else {
                     fieldName = conditionName;
-                    wrapper.eq(value != null, fieldName, value);
+                    wrapper.eq(value != null, fieldNameConvert(fieldName, targetClass), value);
                 }
             }
         }
@@ -98,15 +100,32 @@ public class MybatisPlusRecover {
         Map<String, Boolean> orderByMap = (Map<String, Boolean>) getOrderByMethod.invoke(recoverable);
         if (orderByMap != null) {
             for (Map.Entry<String, Boolean> entry : orderByMap.entrySet()) {
-                if (entry.getValue() == null || entry.getValue()) {
-                    wrapper.orderByAsc(entry.getKey());
+                String fieldName = fieldNameConvert(entry.getKey(), targetClass);
+                Boolean isAscending = entry.getValue();
+                if (isAscending == null || isAscending) {
+                    wrapper.orderByAsc(fieldName);
                 } else {
-                    wrapper.orderByDesc(entry.getKey());
+                    wrapper.orderByDesc(fieldName);
                 }
             }
         }
 
         return wrapper;
+    }
+
+    private static String fieldNameConvert(String fieldName, Class<?> targetClass) {
+        if (!StringUtils.isCamel(fieldName)) {
+            throw new IllegalArgumentException("The field name " + fieldName + " is not camel");
+        }
+        try {
+            TableField tableField = targetClass.getDeclaredField(fieldName).getAnnotation(TableField.class);
+            if (tableField != null) {
+                return tableField.value();
+            }
+            return StringUtils.camelToUnderline(fieldName);
+        } catch (NoSuchFieldException e) {
+            throw new IllegalArgumentException("Not found field name " + fieldName, e);
+        }
     }
 
 }
--- customized-query-core/src/test/java/org/customizedquery/core/support/MybatisPlusRecoverTest.java
+++ customized-query-core/src/test/java/org/customizedquery/core/support/MybatisPlusRecoverTest.java
@@ -1,5 +1,6 @@
 package org.customizedquery.core.support;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.*;
 import org.junit.jupiter.api.Test;
@@ -17,11 +18,15 @@ public class MybatisPlusRecoverTest {
                 .birthday(gt(new Date(0)))
                 .birthday(lt(new Date(0)))
                 .id(between(1L, 10L))
+                .createTime(between(new Date(0), new Date(10000)))
+                .modifiedTime(eq(new Date()))
                 .orderByBirthday(false)
                 .orderById(true)
+                .orderByCreateTime(true)
+                .orderByModifiedTime(false)
                 .build();
 
-        QueryWrapper<A> wrapper = MybatisPlusRecover.recover(aQuery, A.class);
+        QueryWrapper<A> wrapper = MybatisPlusRecover.toWrapper(aQuery, A.class);
         System.out.println(wrapper.getTargetSql());
     }
 
@@ -38,9 +43,17 @@ public class MybatisPlusRecoverTest {
 
         private Range<Date> birthday;
 
+        private Range<Date> createTime;
+
+        private Range<Date> modifiedTime;
+
         private Boolean orderById;
 
         private Boolean orderByBirthday;
+
+        private Boolean orderByCreateTime;
+
+        private Boolean orderByModifiedTime;
 
         public static AQuery.AQueryBuilder queryBuilder() {
             return builderProxy(builder());
@@ -55,6 +68,11 @@ public class MybatisPlusRecoverTest {
         private String name;
 
         private Date birthday;
+
+        @TableField("`create_time`")
+        private Range<Date> createTime;
+
+        private Range<Date> modifiedTime;
 
     }
 
